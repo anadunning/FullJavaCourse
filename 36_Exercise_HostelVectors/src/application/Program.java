d64ccccec51ddadbb4fe94021725d1edd776259a
anadunning <anakpedrosa@hotmail.com>
2023-05-04 11:12:21 -0300

    Exercise 12 to practise Vectors - allocating rented rooms

---
+++ 36_Exercise_HostelVectors/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.UK);
+		Scanner sc = new Scanner(System.in);
+		Rent[] rent = new Rent[10];
+		
+		System.out.print("How many rooms will be rented? ");
+		int n = sc.nextInt();		
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Rent #%d:%n", i + 1);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			rent[room] = new Rent(name, email);
+		}
+		
+		System.out.println("\nBusy rooms:");
+		for (int i = 0; i < 10; i++) {
+			if (rent[i] != null) {
+				System.out.println(i + ": " + rent[i]);
+			}
+		}
+		
+		sc.close();
+	}
+}
