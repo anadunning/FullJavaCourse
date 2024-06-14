710e50e191fa18d7aca0c75dbf218fe31fdb7d41
anadunning <anakpedrosa@hotmail.com>
2024-06-13 23:09:59 -0300

    Using an example to learn Set<T> (TreeSet)

---
+++ 79_Set_TreeSet/src/Program.java
@@ -0,0 +1,21 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Set<String> set = new TreeSet<>();
+		// TreeSet orders the data.
+		
+		set.add("Tv");
+		set.add("Tablet");
+		set.add("Notebook");
+		
+		System.out.println(set.contains("Notebook"));
+		
+		for (String p : set) {
+			System.out.println(p);
+		}
+	}
+}
