15f6a4f48c399fe1e5d7f4566ff97163311079bf
anadunning <anakpedrosa@hotmail.com>
2025-05-20 13:06:40 -0300

    Example of a function receiving a Predicate as a parameter function

---
+++ 100_FunctionsReceivingFunctions_As_Parameter/src/model/services/ProductService.java
@@ -0,0 +1,31 @@
+package model.services;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class ProductService {
+	
+//	public double filteredSum(List<Product> list) {
+//		
+//		double sum = 0.0;
+//		for (Product p : list) {
+//			if (p.getName().charAt(0) == 'T') {
+//				sum += p.getPrice();
+//			}
+//		}
+//		return sum;
+//	}
+	
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+		double sum = 0.0;
+		
+		for (Product p : list) {
+			if (criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}	
+}
